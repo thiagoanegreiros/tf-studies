d96aa0a4615a462f671bdd26da3801dfd03c080d
Thiago Ananias <thiagoananias@gmail.com>
2025-05-08 09:13:58 -0300

    for_each vpc

--- aws/main.tf
+++ aws/main.tf
@@ -29,7 +29,6 @@ resource "aws_security_group" "example" {
     Name = "allow_tls"
   }
 
-
   dynamic "egress" {
     for_each = var.ingress_rules
     content {
@@ -40,4 +39,45 @@ resource "aws_security_group" "example" {
       ipv6_cidr_blocks = ["::/0"]
     }
   }
+}
+
+variable "vpcs" {
+  type = map(object({
+    cidr_block = string
+  }))
+  default = {
+    vpc_01: {
+      cidr_block: "10.0.0.0/16"
+    },
+    vpc_02: {
+      cidr_block: "10.1.0.0/16"
+    }
+
+  }
+}
+
+resource "aws_vpc" "example" {
+  # One VPC for each element of var.vpcs
+  for_each = var.vpcs
+
+  # each.value here is a value from var.vpcs
+  cidr_block = each.value.cidr_block
+}
+
+resource "aws_internet_gateway" "example" {
+  # One Internet Gateway per VPC
+  for_each = aws_vpc.example
+
+  # each.value here is a full aws_vpc object
+  vpc_id = each.value.id
+}
+
+output "vpc_ids" {
+  value = {
+    for k, v in aws_vpc.example : k => v.id
+  }
+
+  # The VPCs aren't fully functional until their
+  # internet gateways are running.
+  depends_on = [aws_internet_gateway.example]
 }
